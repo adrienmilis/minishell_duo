fc5d23546d05eeb6c6d7624f9862d61e3bb1c73c
Hugo Mesnard <hmesnard@fu-r7-p1.s19.be>
2021-06-04 18:56:41 +0200

    normed builtin_unset and launch_executable

--- execution/execution.c
+++ execution/execution.c
@@ -235,30 +235,34 @@ int	builtin_export(char **arg, int pid, int pipes)
 	return (1);
 }
 
+int	builtin_unset_args(char **arg)
+{
+	while (*arg)
+	{
+		if (valid_env_name(*arg, 1))
+		{
+			if (var_is_in_env(myenv, *arg))
+			{
+				myenv = rm_env_var(myenv, *arg);
+				if (!myenv)
+					return (0);
+			}
+		}
+		else
+		{
+			write(2, "minishell: unset: `", 19);
+			write(2, *arg, ft_strlen(*arg));
+			write(2, "\': not a valid identifier\n", 26);
+		}
+		arg++;
+	}
+	return (1);
+}
+
 int	builtin_unset(char **arg, int pid, int pipes)
 {
 	if ((pid != 0 && !pipes) || (pid == 0 && pipes))
-	{
-		while (*arg)
-		{
-			if (valid_env_name(*arg, 1))
-			{
-				if (var_is_in_env(myenv, *arg))
-				{
-					myenv = rm_env_var(myenv, *arg);
-					if (!myenv)
-						return (0);
-				}
-			}
-			else
-			{
-				write(2, "minishell: unset: `", 19);
-				write(2, *arg, ft_strlen(*arg));
-				write(2, "\': not a valid identifier\n", 26);
-			}
-			arg++;
-		}
-	}
+		return (builtin_unset_args(arg));
 	else if (pid == 0)
 		while (*(arg++))
 			if (!valid_env_name(*(arg - 1), 1))
@@ -432,9 +436,54 @@ void	stat_check(char *cmd, t_pipe_cmd *begin, t_PATH *PATH)
 	}
 }
 
+int	launch_executable_p(char **cmd, char *cmd0, t_PATH PATH, t_pipe_cmd *begin)
+{
+	int	i;
+
+	i = 1;
+	while (execve(cmd[0], cmd, myenv + 1) == -1 && PATH.split[i])
+	{
+		if (errno == EACCES && ft_strchr(cmd0, '/'))
+			no_permission(cmd0, begin, &PATH);
+		free(cmd[0]);
+		cmd[0] = ft_strjoin(PATH.split[i++], cmd0, '/');
+		if (!cmd[0])
+			return (0);
+	}
+	if (errno == EACCES && ft_strchr(cmd[0], '/'))
+		no_permission(cmd[0], begin, &PATH);
+	return (1);
+}
+
+int	launch_executable_2(char **cmd, char *cmd0, t_PATH PATH, t_pipe_cmd *begin)
+{
+	if (ft_strchr(cmd[0], '/') || !(PATH.value && PATH.value[0]))
+		execve(cmd[0], cmd, myenv + 1);
+	if (errno == EACCES && ft_strchr(cmd[0], '/'))
+		stat_check(cmd[0], begin, &PATH);
+	cmd0 = cmd[0];
+	if (PATH.value && PATH.value[0] && PATH.split[0])
+	{
+		cmd[0] = ft_strjoin(PATH.split[0], cmd0, '/');
+		if (!cmd[0])
+			return (0);
+	}
+	if (PATH.value && PATH.value[0])
+		if (!launch_executable_p(cmd, cmd0, PATH, begin))
+			return (0);
+	stat_check(cmd0, begin, &PATH);
+	write(2, "minishell: ", 11);
+	write(2, cmd0, ft_strlen(cmd0));
+	if (!ft_strchr(cmd0, '/') && PATH.value && PATH.value[0])
+		write(2, ": command not found\n", 20);
+	else
+		write(2, ": No such file or directory\n", 28);
+	free_exit_exec(127, begin, &PATH);
+	return (1);
+}
+
 int	launch_executable(char **cmd, t_pipe_cmd *begin)
 {
-	int		i;
 	char	*cmd0;
 	t_PATH	PATH;
 
@@ -451,41 +500,8 @@ int	launch_executable(char **cmd, t_pipe_cmd *begin)
 			return (0);
 		}
 	}
-	if (ft_strchr(cmd[0], '/') || !(PATH.value && PATH.value[0]))
-		execve(cmd[0], cmd, myenv + 1);
-	if (errno == EACCES && ft_strchr(cmd[0], '/'))
-		stat_check(cmd[0], begin, &PATH);
-	cmd0 = cmd[0];
-	if (PATH.value && PATH.value[0] && PATH.split[0])
-	{
-		cmd[0] = ft_strjoin(PATH.split[0], cmd0, '/');
-		if (!cmd[0])
-			return (0);
-	}
-	i = 1;
-	if (PATH.value && PATH.value[0])
-	{
-		while (execve(cmd[0], cmd, myenv + 1) == -1 && PATH.split[i])
-		{
-			if (errno == EACCES && ft_strchr(cmd0, '/'))
-				no_permission(cmd0, begin, &PATH);
-			free(cmd[0]);
-			cmd[0] = ft_strjoin(PATH.split[i++], cmd0, '/');
-			if (!cmd[0])
-				return (0);
-		}
-		if (errno == EACCES && ft_strchr(cmd[0], '/'))
-			no_permission(cmd[0], begin, &PATH);
-	}
-	stat_check(cmd0, begin, &PATH);
-	write(2, "minishell: ", 11);
-	write(2, cmd0, ft_strlen(cmd0));
-	if (!ft_strchr(cmd0, '/') && PATH.value && PATH.value[0])
-		write(2, ": command not found\n", 20);
-	else
-		write(2, ": No such file or directory\n", 28);
-	free_exit_exec(127, begin, &PATH);
-	return (1);
+	cmd0 = NULL;
+	return (launch_executable_2(cmd, cmd0, PATH, begin));
 }
 
 int	ctrl_execution(int new_value)
