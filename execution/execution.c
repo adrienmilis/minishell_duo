10c67e25d09aa6c5376be0427eb2fdc257f914ae
Hugo Mesnard <hmesnard@fu-r7-p1.s19.be>
2021-06-04 20:46:47 +0200

    Norm exec_pipe_cmd 2

--- execution/execution.c
+++ execution/execution.c
@@ -546,7 +546,8 @@ void	canonical_mode(int set)
 		return ;
 }
 
-void	error_input(char *input, t_pipe_cmd *begin, int pipefd[2][2], t_exec_data d)
+void	error_input(char *input, t_pipe_cmd *begin, int pipefd[2][2], \
+		t_exec_data d)
 {
 	write(2, "minishell: ", 11);
 	write(2, input, ft_strlen(input));
@@ -581,7 +582,8 @@ int	error_exec(t_pipe_cmd *beg, int pipefd[2][2], t_exec_data d)
 	return (0);
 }
 
-int	redirections(t_pipe_cmd *pipe_cmd, t_pipe_cmd *begin, int fd[3][2], t_exec_data d)
+int	redirections(t_pipe_cmd *pipe_cmd, t_pipe_cmd *begin, int fd[3][2], \
+		t_exec_data d)
 {
 	if (!d.firstcmd && !pipe_cmd->input)
 		dup2(fd[0][0], STDIN_FILENO);
@@ -633,6 +635,47 @@ int	end_execution(int *backslash, int fd[3][2], int status)
 	return (1);
 }
 
+int	exec_simple_cmd(t_pipe_cmd **pipe_cmd, t_pipe_cmd *begin, int fd[3][2], \
+		t_exec_data *d)
+{
+	fd[0][0] = fd[1][0];
+	fd[0][1] = fd[1][1];
+	if (pipe(fd[1]) == -1)
+		return (error_exec(begin, fd, *d));
+	signal(SIGINT, &handler_signal_execution);
+	signal(SIGQUIT, &handler_signal_execution);
+	d->pid = fork();
+	if (d->pid == -1)
+		return (error_exec(begin, fd, *d));
+	if (d->pid == 0)
+		if (!redirections(*pipe_cmd, begin, fd, *d))
+			return (0);
+	if (!d->firstcmd)
+	{
+		close(fd[0][0]);
+		close(fd[0][1]);
+	}
+	if ((*pipe_cmd)->cmd)
+	{
+		if (is_builtin((*pipe_cmd)->cmd))
+		{
+			if (!do_builtin((*pipe_cmd)->cmd, d->pid, d->pipes))
+				return (error_exec(begin, fd, *d));
+		}
+		else if (d->pid == 0)
+			if (!launch_executable((*pipe_cmd)->cmd, begin))
+				return (error_exec(begin, fd, *d));
+	}
+	if (d->pid == 0)
+		free_exit_exec(exit_status(0, d->pid, 0), begin, NULL);
+	waitpid(d->pid, &d->status, 0);
+	if (exit_status(0, 0, 1))
+		free_exit_exec(exit_status(0, d->pid, 0), begin, NULL);
+	*pipe_cmd = (*pipe_cmd)->next;
+	d->firstcmd = 0;
+	return (1);
+}
+
 int	exec_pipe_cmd(t_pipe_cmd *pipe_cmd, int *backslash)
 {
 	t_pipe_cmd	*begin;
@@ -648,42 +691,7 @@ int	exec_pipe_cmd(t_pipe_cmd *pipe_cmd, int *backslash)
 		d.pipes = 1;
 	canonical_mode(1);
 	while (pipe_cmd && !ctrl_execution(1) && (pipe_cmd->cmd || pipe_cmd->input))
-	{
-		fd[0][0] = fd[1][0];
-		fd[0][1] = fd[1][1];
-		if (pipe(fd[1]) == -1)
-			return (error_exec(begin, fd, d));
-		signal(SIGINT, &handler_signal_execution);
-		signal(SIGQUIT, &handler_signal_execution);
-		d.pid = fork();
-		if (d.pid == -1)
-			return (error_exec(begin, fd, d));
-		if (d.pid == 0)
-			if (!redirections(pipe_cmd, begin, fd, d))
-				return (0);
-		if (!d.firstcmd)
-		{
-			close(fd[0][0]);
-			close(fd[0][1]);
-		}
-		if (pipe_cmd->cmd)
-		{
-			if (is_builtin(pipe_cmd->cmd))
-			{
-				if (!do_builtin(pipe_cmd->cmd, d.pid, d.pipes))
-					return (error_exec(begin, fd, d));
-			}
-			else if (d.pid == 0)
-				if (!launch_executable(pipe_cmd->cmd, begin))
-					return (error_exec(begin, fd, d));
-		}
-		if (d.pid == 0)
-			free_exit_exec(exit_status(0, d.pid, 0), begin, NULL);
-		waitpid(d.pid, &d.status, 0);
-		if (exit_status(0, 0, 1))
-			free_exit_exec(exit_status(0, d.pid, 0), begin, NULL);
-		pipe_cmd = pipe_cmd->next;
-		d.firstcmd = 0;
-	}
+		if (!exec_simple_cmd(&pipe_cmd, begin, fd, &d))
+			return (0);
 	return (end_execution(backslash, fd, d.status));
 }
